baed7bf668c9623ad33f776e692dbd5efbd139a4
codefaring <codefaring@gmail.com>
2023-01-08 23:43:29 +0900

    react input test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState({});
+  const [inputInfo, setInputInfo] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({
+      name: '',
+      phoneNum: '',
+    });
+    setInputInfo({
+      name: user.name,
+      phoneNum: user.phoneNum,
+    });
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>이름</label>
+        <input
+          type='text'
+          value={user.name}
+          name='name'
+          onChange={handleChange}
+        />
+        <label>전화번호</label>
+        <input
+          type='text'
+          value={user.phoneNum}
+          name='phoneNum'
+          onChange={handleChange}
+        />
+        <button>전송</button>
+        <h3>입력값</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          이름: {inputInfo.name}
+          <br />
+          전화번호: {inputInfo.phoneNum}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </form>
     </div>
   );
 }
-
-export default App;
